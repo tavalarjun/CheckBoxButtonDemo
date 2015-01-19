0ab0789546546a5cb2bc3ca3ab06910dd25531e4
Omkar Jadhav <omkar.jadhav@gmail.com>
2015-01-19 15:15:16 +0530

    Initial Commit

---
+++ checkandradiobuttonDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  checkandradiobuttonDemo
+//
+//  Created by Dropouts on 1/19/15.
+//  Copyright (c) 2015 Dropouts Technologies LLP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
